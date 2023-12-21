b6863baac19749511681a8d304a12a5baa7a688a
natasha8775735@gmail.com <natalliakhadakouskaya>
2023-12-21 11:45:07 +0300

    HomeWork06

---
+++ src/main/java/HomeWork06/view/UserView.java
@@ -0,0 +1,10 @@
+package HomeWork06.view;
+
+import HomeWork04.model.User;
+
+import java.util.List;
+
+// Интерфейс работает для всех классов, которые наследуются от Юзер
+public interface UserView<T extends User>{
+    void sendOnConsole(List<T> list);
+}
